20a47e4a5c927296a8fe5966c89d73d590a12b14
unknown <Teamprojekt@vrpc01.in.fhkn.de>
2018-02-21 11:40:52 +0100

    Added first Avatar with movement scripts

---
+++ Assets/Scripts/AvatarController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarController : MonoBehaviour {
+    #region Public Variables
+    public Transform cameraRigTransform;
+    public Transform headTransform;
+    public Transform cameraAnchorPoint;
+
+    public float rotationBorder = 60f;
+    #endregion
+
+    #region Private Variables
+    private Transform avatarTransform;
+    private float avatarRotationY;
+    #endregion
+
+    #region MonoBehaviour Callbacks
+    private void Awake()
+    {
+        avatarTransform = this.gameObject.transform;
+        avatarRotationY = avatarTransform.rotation.eulerAngles.y;
+    }
+
+    // Use this for initialization
+    void Start () {
+        PlaceRig();		
+	}
+
+    private void FixedUpdate()
+    {
+        RotateBody();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        FollowCamera();
+    }
+    #endregion
+
+    #region Private Methods
+    /// <summary>
+    /// This places the camera rig to the position of the avatar
+    /// </summary>
+    private void PlaceRig()
+    {
+        Vector3 difference = cameraRigTransform.position - headTransform.position;
+        difference.y = 0;
+
+        Vector3 anchorPoint = cameraAnchorPoint.position;
+        anchorPoint.y = cameraRigTransform.position.y;
+        cameraRigTransform.position = anchorPoint + difference;
+    }
+
+    /// <summary>
+    /// The avatar follows the position and also the height of the camera.
+    /// </summary>
+    private void FollowCamera()
+    {
+        Vector3 difference = avatarTransform.position - cameraAnchorPoint.position;
+
+        avatarTransform.position = headTransform.position + difference;
+    }
+
+    private void RotateBody()
+    {
+        Quaternion headRotation = avatarTransform.Find("Armature/Belly/Chest/Neck/Head").rotation;
+        float headRotationAngleY = headRotation.eulerAngles.y;
+
+        if (headRotationAngleY <= (avatarRotationY - rotationBorder) || headRotationAngleY >= (avatarRotationY + rotationBorder))
+        {
+            avatarTransform.eulerAngles = new Vector3(0, headRotationAngleY, 0);
+            avatarRotationY = avatarTransform.rotation.eulerAngles.y;
+        }
+    }
+    #endregion
+}
---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour {
+    protected Animator animator;
+
+    public Transform headLookObject;
+    public Transform leftHandObject;
+    public Transform rightHandObject;
+
+    private Dictionary<Transform, AvatarIKGoal> bodyDict;
+
+    private void TrackHead()
+    {
+        if (headLookObject)
+        {
+            animator.SetLookAtWeight(1);
+            animator.SetLookAtPosition(headLookObject.position);
+        }
+    }
+
+    private void TrackBodyPart(Transform bodyPart)
+    {
+        AvatarIKGoal goal;
+        if (bodyPart && bodyDict.TryGetValue(bodyPart, out goal))
+        {
+            animator.SetIKPositionWeight(goal, 1);
+            animator.SetIKRotationWeight(goal, 1);
+            animator.SetIKPosition(goal, bodyPart.position);
+            animator.SetIKRotation(goal, bodyPart.rotation);
+        }
+    }
+
+    private void Awake()
+    {
+        bodyDict = new Dictionary<Transform, AvatarIKGoal>();
+        bodyDict.Add(leftHandObject, AvatarIKGoal.LeftHand);
+        bodyDict.Add(rightHandObject, AvatarIKGoal.RightHand);
+    }
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        if (!animator)
+        {
+            return;
+        }
+
+        TrackHead();
+
+        TrackBodyPart(leftHandObject);
+        TrackBodyPart(rightHandObject);
+    }
+}
